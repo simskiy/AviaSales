34c623d61d6756619e490a326f9a877bc2c8be3b
simskiy <simskiy@gmail.com>
2022-12-11 22:03:40 +0300

    настройка стостояния для фильтра. Привязка чекбоксов к стейту

--- src/components/filterTrransfer/FilterTransfer.tsx
+++ src/components/filterTrransfer/FilterTransfer.tsx
@@ -1,7 +1,10 @@
 import React from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { filterAll, filterNothing, filterRef1, filterRef2, filterRef3 } from "../../store/filterSlice";
 import styles from './FilterTransfer.module.scss'
 
 const transfer = ['Все', 'Без пересадок', '1 пересадка', '2 пересадки', '3 пересадки']
+const actions = [filterAll, filterNothing, filterRef1, filterRef2, filterRef3]
 interface IFilterProps {
   label: string,
   ind: number
@@ -17,9 +20,18 @@ export default function FilterTransfer () {
 }
 
 const FilterItem: React.FC<IFilterProps> = ({label, ind}) => {
+  const state = useSelector((state: any) => state.filterTicket)
+  const keys = Object.keys(state)
+  const dispatch = useDispatch()
   return (
     <p className={styles.filter__item}>
-      <input type="checkbox" id={`item-${ind}`} className={styles.filter__input} />
+      <input
+        type="checkbox"
+        id={`item-${ind}`}
+        className={styles.filter__input}
+        onChange={() => dispatch(actions[ind]())}
+        checked = {state[keys[ind]]}
+      />
       <label htmlFor={`item-${ind}`} className={styles.filter__label}>{label}</label>
     </p>
   )
--- src/store/index.ts
+++ src/store/index.ts
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 
 import sortReducer from "./sortSlice";
+import filterReducer from "./filterSlice"
 
 export default configureStore({
     reducer: {
-        sortTicket: sortReducer
+        sortTicket: sortReducer,
+        filterTicket: filterReducer
     }
 })
